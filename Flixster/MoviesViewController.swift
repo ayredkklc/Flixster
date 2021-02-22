a051ed19a7fb773356ced6262b93d63ebb1ac0fd
deryakilic <ayredkklc@unc.edu>
2021-02-21 23:48:48 -0500

    hi

--- Flixster/MoviesViewController.swift
+++ Flixster/MoviesViewController.swift
@@ -41,6 +41,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
+            print("hi")
 
            }
         }
