2032fdd9f89fac0ab3f726355c19e642f5df5b32
deryakilic <ayredkklc@unc.edu>
2021-02-27 00:42:15 -0500

    hi

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Derya Kilic on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flixster/MovieGridViewController.swift
+++ Flixster/MovieGridViewController.swift
@@ -1,28 +1,38 @@
 //
-//  MoviesViewController.swift
+//  MovieGridViewController.swift
 //  Flixster
 //
-//  Created by Derya Kilic on 2/19/21.
+//  Created by Derya Kilic on 2/26/21.
 //
 
 import UIKit
 import AlamofireImage
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+class MovieGridViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     
     
-    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var collentionView: UICollectionView!
     var movies = [[String:Any]]()
 
+    @IBOutlet weak var posterView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
-        tableView.delegate = self
+        collentionView.delegate = self
+        collentionView.dataSource = self
         
+        let layout = collentionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
+        layout.itemSize = CGSize(width: width, height: width * 3/2)
+        
+
         // Do any additional setup after loading the view.
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -36,7 +46,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             //sometimes accesing the variables you need self the compiler will tell you
             //movies = dataDictionary["results"] as! [[String:Any]] to the next line is how xcode fixes it
             self.movies = dataDictionary["results"] as! [[String:Any]]
-            self.tableView.reloadData()
+            self.collentionView.reloadData()
+            //self.tableView.reloadData()
             
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
@@ -47,52 +58,34 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         }
         task.resume()
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return movies.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
         
-        let movie = movies[indexPath.row]
-        let title = movie["title"] as! String
-        let synopsis = movie["overview"] as! String
-        
-        //cell.textLabel!.text = title
-        cell.titleLabel.text = title
-        cell.synopsisLabel.text = synopsis
-        // because of swift optional just like casting it is adding ? before .text
+        let movie = movies[indexPath.item]
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         //cell.posterView.af_setImage(withURL: posterUrl!) but it changed it to below idk why
-        cell.posterView.af.setImage(withURL: posterUrl!)
+        cell.posterView.af_setImage(withURL: posterUrl!)
+        
+        
         return cell
     }
     
 
- 
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-        
-        //find th eselcted value
-        let cell = sender as! UITableViewCell
-        let indexPath = tableView.indexPath(for: cell)!
-        let movie = movies[indexPath.row]
-      
-        //pass th eselected movie details to the details movie controller
-        let detailsViewControllor = segue.destination as! MoviesDetailsViewController
-        detailsViewControllor.movie = movie
-        
-        //toi deselct the aminated press in app
-        tableView.deselectRow(at: indexPath, animated: true)
     }
-    
+    */
 
 }
